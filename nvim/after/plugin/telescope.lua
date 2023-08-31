7b7cf2c88a394ee009ae32f9475d3c22dd0a77fe
Andrew Halberstadt <ahal@pm.me>
2023-08-31 15:34:21 -0400

    Add keymap for grepping in current buffer's parent

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -32,5 +32,6 @@ vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { des
 vim.keymap.set('n', '<leader>sF', require('telescope.builtin').git_files, { desc = '[S]earch [F]iles [G]it' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
+vim.keymap.set('n', '<leader>sg', require('telescope').extensions.live_grep_args.live_grep_args, { desc = '[S]earch by [G]rep' })
+vim.keymap.set('n', '<leader>sG', function() require('telescope.builtin').live_grep{search_dirs = {vim.fn.expand('%:p:h')}} end, { desc = '[S]earch by [G]rep in Current Buffer Parent' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
