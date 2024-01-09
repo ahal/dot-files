f5857e01e65d5b9b1150c272c460d22ed4c207d8
Andrew Halberstadt <ahal@pm.me>
2024-01-09 11:39:26 -0500

    Update configs

--- nvim/lua/plugins/ctrlsf.lua
+++ nvim/lua/plugins/ctrlsf.lua
@@ -1,6 +1,9 @@
 return {
   'dyng/ctrlsf.vim',
   config = function()
-    vim.keymap.set('n', '<leader>sa', '<Plug>CtrlSFPrompt', { desc = '[S]earch [A]cross project' })
+    vim.keymap.set('n', '<leader>ff', '<Plug>CtrlSFPrompt', { desc = 'CtrlSFPrompt' })
+    vim.keymap.set('v', '<leader>ff', '<Plug>CtrlSFVwordPath', { desc = 'CtrlSFVwordPath' })
+    vim.keymap.set('n', '<leader>fw', '<Plug>CtrlSFCwordPath<CR>', { desc = 'CtrlSFCwordPath' })
+    vim.keymap.set('n', '<leader>ft', ':CtrlSFToggle<CR>', { desc = 'CtrlSFToggle' })
   end
 }
