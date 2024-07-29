359acfcc93a5c15475866fd857cf04dd4fc114f1
Andrew Halberstadt <ahal@pm.me>
2024-07-29 11:46:55 -0400

    Update configs

---
+++ dot-files/zshrc.d/03-alias.sh
@@ -0,0 +1,20 @@
+# aliases
+alias cd="pushd -q"
+alias ag="ag --path-to-agignore ~/.ignore"
+alias dc="docker compose"
+alias indent="python -m json.tool"
+alias isitmfbt="wget -qO - isitmfbt.com | grep 'theTime' | sed -e 's/<[^>]*>//g'"
+alias jsonsort="jq -S -f ~/bin/sort.jq"
+#alias ls="exa"
+alias pg="ps -ef | grep"
+alias pysite="python -c 'import site; print(site.getsitepackages()[0])'"
+alias rg="rg --ignore-file $HOME/.ignore --hidden"
+alias usage="du -h --max-depth=1 2> /dev/null | sort -h -r | head -n20"
+alias nvim="flatpak run io.neovim.nvim"
+alias which="pyenv which"
+
+
+if $IS_WSL; then
+  alias xclip="xclip -selection clipboard"
+fi
+
