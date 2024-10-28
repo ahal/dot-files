f036b321b907ce6be85bfaef50a4c667f202ae31
Andrew Halberstadt <ahal@mozilla.com>
2024-10-28 12:31:02 -0400

    Update configs

--- dot-files/zshrc.d/09-completions.zsh
+++ dot-files/zshrc.d/09-completions.zsh
@@ -1,14 +1,4 @@
 autoload -U bashcompinit
 bashcompinit
 
-#autoload -U compinit
-#compinit
-#source <(jj util completion zsh)
-
-# argc-completions
-export ARGC_COMPLETIONS_ROOT="/home/ahal/dev/argc-completions"
-export ARGC_COMPLETIONS_PATH="$ARGC_COMPLETIONS_ROOT/completions/linux:$ARGC_COMPLETIONS_ROOT/completions"
-export PATH="$ARGC_COMPLETIONS_ROOT/bin:$PATH"
-# To add completions for only the specified command, modify next line e.g. argc_scripts=( cargo git )
-argc_scripts=( $(ls -p -1 "$ARGC_COMPLETIONS_ROOT/completions/linux" "$ARGC_COMPLETIONS_ROOT/completions" | sed -n 's/\.sh$//p') )
-source <(argc --argc-completions zsh $argc_scripts)
+source <(COMPLETE=zsh jj)
