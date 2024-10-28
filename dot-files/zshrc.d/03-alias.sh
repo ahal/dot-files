7771b74f05a3ac90c5c40fc8a729b78272613c71
Andrew Halberstadt <ahal@mozilla.com>
2024-10-28 12:31:02 -0400

    Update configs

--- dot-files/zshrc.d/03-alias.sh
+++ dot-files/zshrc.d/03-alias.sh
@@ -5,7 +5,7 @@ alias dc="docker compose"
 alias indent="python -m json.tool"
 alias isitmfbt="wget -qO - isitmfbt.com | grep 'theTime' | sed -e 's/<[^>]*>//g'"
 alias jsonsort="jq -S -f ~/bin/sort.jq"
-alias ls="exa"
+#alias ls="exa"
 alias pg="ps -ef | grep"
 alias pysite="python -c 'import site; print(site.getsitepackages()[0])'"
 alias rg="rg --ignore-file $HOME/.ignore --hidden"
@@ -14,6 +14,9 @@ alias usage="du -h --max-depth=1 2> /dev/null | sort -h -r | head -n20"
 # flatpaks
 alias meld="org.gnome.meld"
 alias nvim="io.neovim.nvim"
+
+# jujutsu
+alias jj-push="pre-commit run -a && jj git push"
 
 if $IS_WSL; then
   alias xclip="xclip -selection clipboard"
