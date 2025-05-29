904c65fdbcc6a7237413448123c80ef8fe3b7690
Andrew Halberstadt <ahal@mozilla.com>
2025-05-29 09:32:32 -0400

    feat: add custom callback for searchfox to gitlinker

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -9,6 +9,28 @@ return {
       require('gitlinker').setup({
         opts = {
           remote = 'origin',
+        },
+        callbacks = {
+          ["github.com"] = function(url_data)
+            -- Check if this is the Mozilla Firefox repo
+            if url_data.repo == "mozilla-firefox/firefox" then
+              -- Use searchfox.org for Firefox
+              local url = "https://searchfox.org/firefox-main/rev/"
+                .. url_data.rev .. "/"
+                .. url_data.file
+              if url_data.lstart then
+                url = url .. "#" .. url_data.lstart
+                if url_data.lend and url_data.lend ~= url_data.lstart then
+                  url = url .. "-" .. url_data.lend
+                end
+              end
+              return url
+            else
+              -- Use the built-in GitHub callback
+              local hosts = require('gitlinker.hosts')
+              return hosts.get_github_type_url(url_data)
+            end
+          end
         }
       })
     end
