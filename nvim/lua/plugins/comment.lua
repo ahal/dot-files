3d172a6d1f75b0663972a79b81922e6e96ed66c7
Andrew Halberstadt <andrew@ahal.ca>
2026-04-18 23:13:10 -0400

    fix: clean nvim configs for 0.12

--- nvim/lua/plugins/comment.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    'numToStr/Comment.nvim', -- "gc" to comment visual regions/lines
-    config = function()
-      require('Comment').setup()
-    end
-  },
-  { 'tpope/vim-commentary' },
-}
